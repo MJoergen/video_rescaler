ea40a5c73fed1943d94fabf2c73d868479efdf39
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2022-02-22 07:02:28 +0100

    Change input video to PAL 720x576 @ 50 Hz

--- democore.vhd
+++ democore.vhd
@@ -7,18 +7,18 @@ use unisim.vcomponents.all;
 
 entity democore is
    generic (
-      CLK_KHZ   : integer := 25000;     -- 25.00 MHz
+      CLK_KHZ   : integer := 27000;     -- 25.00 MHz
       PIX_SIZE  : integer := 11;
-      H_PIXELS  : integer :=  640;      -- horizontal display width in pixels
-      V_PIXELS  : integer :=  480;      -- vertical display width in rows
-      H_FP      : integer :=   16;      -- horizontal front porch width in pixels
-      H_PULSE   : integer :=   96;      -- horizontal sync pulse width in pixels
-      H_BP      : integer :=   48;      -- horizontal back porch width in pixels
-      V_FP      : integer :=   10;      -- vertical front porch width in rows
-      V_PULSE   : integer :=    2;      -- vertical sync pulse width in rows
-      V_BP      : integer :=   33;      -- vertical back porch width in rows
-      H_MAX     : integer :=  800;
-      V_MAX     : integer :=  525;
+      H_PIXELS  : integer :=  720;      -- horizontal display width in pixels
+      V_PIXELS  : integer :=  576;      -- vertical display width in rows
+      H_FP      : integer :=   17;      -- horizontal front porch width in pixels
+      H_PULSE   : integer :=   64;      -- horizontal sync pulse width in pixels
+      H_BP      : integer :=   63;      -- horizontal back porch width in pixels
+      V_FP      : integer :=    5;      -- vertical front porch width in rows
+      V_PULSE   : integer :=    5;      -- vertical sync pulse width in rows
+      V_BP      : integer :=   39;      -- vertical back porch width in rows
+      H_MAX     : integer :=  864;
+      V_MAX     : integer :=  625;
       H_POL     : std_logic := '1';       -- horizontal sync pulse polarity (1 = positive, 0 = negative)
       V_POL     : std_logic := '1'        -- vertical sync pulse polarity (1 = positive, 0 = negative)
    );
@@ -49,6 +49,14 @@ architecture synthesis of democore is
    constant C_HS_START : integer := H_PIXELS + H_FP;
    constant C_VS_START : integer := V_PIXELS + V_FP;
 
+   constant C_BORDER  : integer := 4;  -- Number of pixels
+   constant C_SQ_SIZE : integer := 50; -- Number of pixels
+
+   signal pos_x : integer range 0 to H_PIXELS-1 := H_PIXELS/2;
+   signal pos_y : integer range 0 to V_PIXELS-1 := V_PIXELS/2;
+   signal vel_x : integer range -7 to 7         := 1;
+   signal vel_y : integer range -7 to 7         := 1;
+
 begin
 
    vga_clk_o <= vga_clk;
@@ -63,11 +71,11 @@ begin
          STARTUP_WAIT         => FALSE,
          CLKIN1_PERIOD        => 10.0,       -- INPUT @ 100 MHz
          REF_JITTER1          => 0.010,
-         DIVCLK_DIVIDE        => 5,
-         CLKFBOUT_MULT_F      => 37.125,     -- f_VCO = (100 MHz / 5) x 37.125 = 742.5 MHz
+         DIVCLK_DIVIDE        => 1,
+         CLKFBOUT_MULT_F      => 10.125,    -- f_VCO = (100 MHz / 1) x 10.125 = 1012.5 MHz
          CLKFBOUT_PHASE       => 0.000,
          CLKFBOUT_USE_FINE_PS => FALSE,
-         CLKOUT0_DIVIDE_F     => 29.500,     -- i_clk @ 25.17 MHz
+         CLKOUT0_DIVIDE_F     => 37.500,     -- i_clk @ 27.00 MHz
          CLKOUT0_PHASE        => 0.000,
          CLKOUT0_DUTY_CYCLE   => 0.500,
          CLKOUT0_USE_FINE_PS  => FALSE
@@ -192,14 +200,61 @@ begin
       end if;
    end process p_sync;
 
+
    p_rgb : process (vga_clk)
    begin
       if rising_edge(vga_clk) then
-         vga_r_o <= std_logic_vector(to_unsigned(to_integer(unsigned(pixel_x)) mod 256, 8));
-         vga_g_o <= std_logic_vector(to_unsigned(to_integer(unsigned(pixel_y)) mod 256, 8));
-         vga_b_o <= std_logic_vector(to_unsigned(to_integer(unsigned(pixel_x)) + to_integer(unsigned(pixel_y)) mod 256, 8));
+         -- Render background
+         vga_r_o <= X"88";
+         vga_g_o <= X"CC";
+         vga_b_o <= X"AA";
+
+         -- Render white border
+         if unsigned(pixel_x) < C_BORDER or unsigned(pixel_x) + C_BORDER >= H_PIXELS or
+            unsigned(pixel_y) < C_BORDER or unsigned(pixel_y) + C_BORDER >= V_PIXELS then
+            vga_r_o <= X"FF";
+            vga_g_o <= X"FF";
+            vga_b_o <= X"FF";
+         end if;
+
+         -- Render red-ish square
+         if unsigned(pixel_x) >= pos_x and unsigned(pixel_x) < pos_x + C_SQ_SIZE and
+            unsigned(pixel_y) >= pos_y and unsigned(pixel_y) < pos_y + C_SQ_SIZE then
+            vga_r_o <= X"EE";
+            vga_g_o <= X"20";
+            vga_b_o <= X"40";
+         end if;
       end if;
    end process p_rgb;
+
+
+   -- Move the square
+   p_move : process (vga_clk)
+   begin
+      if rising_edge(vga_clk) then
+         -- Update once each frame
+         if unsigned(pixel_x) = 0 and unsigned(pixel_y) = 0 then
+            pos_x <= pos_x + vel_x;
+            pos_y <= pos_y + vel_y;
+
+            if pos_x >= H_PIXELS - C_SQ_SIZE - C_BORDER and vel_x > 0 then
+               vel_x <= -vel_x;
+            end if;
+
+            if pos_x < C_BORDER and vel_x < 0 then
+               vel_x <= -vel_x;
+            end if;
+
+            if pos_y >= V_PIXELS - C_SQ_SIZE - C_BORDER and vel_y > 0 then
+               vel_y <= -vel_y;
+            end if;
+
+            if pos_y < C_BORDER and vel_y < 0 then
+               vel_y <= -vel_y;
+            end if;
+         end if;
+      end if;
+   end process p_move;
 
 end architecture synthesis;
 
